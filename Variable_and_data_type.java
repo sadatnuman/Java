914e51f157e0bc15ca632583cb18840f3bfc7c5a
MD. Nazmus Sadat Numan <116034911+sadatnuman@users.noreply.github.com>
2022-12-26 16:19:18 +0600

    Add files via upload

---
+++ Variable_and_data_type.java
@@ -0,0 +1,22 @@
+public class Variable_and_data_type {
+    public static void main(String[] args){
+        boolean B = true;
+        char C = 'N';
+        byte b = 10;
+        short S = 11;
+        int I = 12;
+        long L = 13;
+        float F = 14.12f;
+        double D = 15.00;
+        System.out.println("\n\n\n");
+        System.out.println("Boolean : "+B);
+        System.out.println("Char : "+C);
+        System.out.println("Byte : "+b);
+        System.out.println("Short : "+S);
+        System.out.println("Int : "+I);
+        System.out.println("Long : "+L);
+        System.out.println("Float : "+F);
+        System.out.println("Double : "+D);
+        System.out.println("\n\n\n");
+    }
+}
