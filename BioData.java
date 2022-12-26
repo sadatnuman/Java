574a8228040faf65505a01b8a056ba70d8f702d0
MD. Nazmus Sadat Numan <116034911+sadatnuman@users.noreply.github.com>
2022-12-26 14:46:12 +0600

    Add files via upload

---
+++ BioData.java
@@ -0,0 +1,9 @@
+public class BioData {
+    public static void main(String[] args){
+        System.out.println("Name : MD. Nazmus Sadat Numan");
+        System.out.print("Age : 20\n");
+        System.out.println("ADD : Pangsha , Rajbari");
+        System.out.println("Mobile : 01645489811");
+        System.out.println("Email : nazmussadatnuman92@gmail.com");
+    }
+}
