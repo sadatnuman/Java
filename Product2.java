cdbd2eab4142b25c23c6b4efd4fdb262d6e8fd86
MD. Nazmus Sadat Numan <116034911+sadatnuman@users.noreply.github.com>
2022-12-26 16:36:47 +0600

    Add files via upload

---
+++ Product2.java
@@ -0,0 +1,16 @@
+public class Product2 {
+    public static void main(String[] args){
+        int id = 101;
+        String title = "Iphone 15";
+        int price = 1895;
+        String description = "Perfect product with best image quality.";
+        String category = "Phone.";
+        System.out.print("\n\n");
+        System.out.println("ID : "+id);
+        System.out.println("Title : "+title);
+        System.out.println("Price : "+price);
+        System.out.println("Description : "+description);
+        System.out.println("Category : "+category);
+        System.out.print("\n\n");
+    }
+}
