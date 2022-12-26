96eb35e1ef3077c5cf885c48c768ff12c1511dfb
MD. Nazmus Sadat Numan <116034911+sadatnuman@users.noreply.github.com>
2022-12-26 15:08:41 +0600

    Add files via upload

---
+++ Escape_Sequence.java
@@ -0,0 +1,11 @@
+public class Escape_Sequence {
+    public static void main(String[] args){
+        System.out.println("MD. Nazmus \bSadat \bNuman");                      //MD. NazmusSadatNuman                      Back space
+        System.out.println("Age: \t20");                                       //Age:    20                                Tab
+        System.out.print("01645489811\n");                                     //01645489811                               Newline
+        System.out.println("email \rEmail: nazmussadatnuman92@gmaol.com");     //Email: nazmussadatnuman92@gmaol.com       Carriage return
+        System.out.println("\"Rajbari\"");                                     //"Rajbari"                                 Double quote
+        System.out.println("\'Pangsha\'");                                     //'Pangsha'                                 Single quote
+        System.out.println("Email\\Phone");                                    //Email\Phone                               Back Slash
+    }
+}
