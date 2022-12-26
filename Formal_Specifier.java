407107a7f09f5b759f1581591bcaeabb004b5cc7
MD. Nazmus Sadat Numan <116034911+sadatnuman@users.noreply.github.com>
2022-12-26 17:05:05 +0600

    Add files via upload

---
+++ Formal_Specifier.java
@@ -0,0 +1,24 @@
+public class Formal_Specifier {
+    public static void main(String[] args){
+        boolean B = true;
+        char C = 'N';
+        byte b = 10;
+        short S = 11;
+        int I = 12;
+        long L = 13;
+        float F = 14.12f;
+        double D = 15.00;
+        System.out.println("\n\n\n");
+        System.out.printf("Boolean : %B\n",B);
+        System.out.printf("Char : %c\n",C);
+        System.out.printf("Byte : %d\n",b);
+        System.out.printf("Short : %d\n",S);
+        System.out.printf("Int : %d\n",I);
+        System.out.printf("Long : %d\n",L);
+        System.out.printf("Float : %f\n",F);                   
+        //System.out.printf("Float : %.2f\n",F);
+        System.out.printf("Double : %f\n",D);               
+        //System.out.printf("Double : %.3f\n",D);
+        System.out.println("\n\n\n");
+    }
+}
