6fa7209c843f6f60c13994ff283b906332409f1f
MD. Nazmus Sadat Numan <116034911+sadatnuman@users.noreply.github.com>
2023-01-01 21:19:59 +0600

    Add files via upload

---
+++ product5.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class product5 {
+	public static void main(String[] args) {
+		System.out.print("\n\n\n");
+        Scanner input = new Scanner(System.in);
+		int voterAge;
+		System.out.print("Enter your age: ");
+		voterAge = input.nextInt();
+		if (voterAge >= 18) {
+			System.out.println("Valid Voter");
+		} 
+        else {
+			System.out.println("Invalid Voter");
+		}
+        System.out.print("\n\n\n");
+	}
+}
