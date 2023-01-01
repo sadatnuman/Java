1014b05903b51021db62a2760372868a28bfb708
MD. Nazmus Sadat Numan <116034911+sadatnuman@users.noreply.github.com>
2023-01-01 21:36:29 +0600

    Update and rename product5.java to Product5.java

--- Product5.java
+++ Product5.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class product5 {
+public class Product5 {
 	public static void main(String[] args) {
 		System.out.print("\n\n\n");
         Scanner input = new Scanner(System.in);
